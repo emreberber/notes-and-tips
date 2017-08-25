c27a3be3b5c2aa19464d29a048ff105eb23448d2
Emre Berber <brbr.emre@gmail.com>
2017-08-25 18:46:07 +0300

    Php dosyası olusturuldu

---
+++ Php/DB_connect.php
@@ -0,0 +1,17 @@
+<?php
+
+
+// VERİTABANI BAGLANTISI
+
+try
+{
+	$db=new PDO("mysql:host=localhost;dbname=veritabanı;charset=utf8",'kullanıcı','sifre');
+//	echo "baglanti basarili";
+}
+catch(PDOException $e)
+{
+	echo $e->getMessage();
+}
+
+
+?>

dae5d2399997b399b1a306be7edfccaf0c7a3f0f
Emre Berber <brbr.emre@gmail.com>
2017-08-25 18:21:54 +0300

    Random.cs

---
+++ CSharp/Random.cs
@@ -0,0 +1,31 @@
+﻿/*
+><><><><><><><><><><><><><><><><><><><><><><><><><><><
+>													 
+>              Developer :  Emre Berber
+>		Filename :  Random.cs
+>		   Other :  emreberber.com 
+>
+><><><><><><><><><><><><><><><><><><><><><><><><><><><
+*/
+
+
+using System;
+
+
+namespace Random_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Random Rnd = new Random();
+
+            int rnd1 = Rnd.Next(7, 70);    //  7 ile 70 arası
+            int rnd2 = Rnd.Next(61);    //  0 ile 61 arası
+            int rnd3 = Rnd.Next();  //  int türünden pozitif
+            double rnd4 = Rnd.NextDouble(); //  0.0 ile 1 arası double
+
+        }
+    }
+}

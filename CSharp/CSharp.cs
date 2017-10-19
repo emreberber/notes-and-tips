2b6c472e8fc5cde1e34cfcb7b99820d0e7c54d02
emreberber <brbr.emre@gmail.com>
2017-10-19 21:27:27 +0300

    OOP-1

---
+++ CSharp/CSharp.cs
@@ -0,0 +1,35 @@
+﻿/*
+><><><><><><><><><><><><><><><><><><><><><><><><><><><
+>													 
+>              Developer :  Emre Berber
+>		Filename :  CSharp.cs
+>		   Other :  emreberber.com 
+>
+><><><><><><><><><><><><><><><><><><><><><><><><><><><
+*/
+
+
+using System;
+
+
+
+namespace CSharp  // Program.cs dosyasında dahil edeğemiz alan
+{
+    public class RastgeleSayi
+    {
+        private Random rnd = new Random();
+        private int mSayi;
+
+        public int Sayi { get { return mSayi; } }
+        
+        public RastgeleSayi()
+        {
+            mSayi = rnd.Next(0, 100);
+        }
+
+        public void Degistir()
+        {
+            mSayi = rnd.Next(0, 100);
+        }
+    }
+}
---
+++ CSharp/OOP_Class.cs
@@ -0,0 +1,29 @@
+﻿/*
+><><><><><><><><><><><><><><><><><><><><><><><><><><><
+>													 
+>              Developer :  Emre Berber
+>		Filename :  OOP_CLass.cs
+>		   Other :  emreberber.com 
+>
+><><><><><><><><><><><><><><><><><><><><><><><><><><><
+*/
+
+
+using System;
+using CSharp;  // Sınıf kütüphanesi kullanma   
+
+
+    class MainMetodu
+    {
+        static void Main()
+        {
+        RastgeleSayi r = new RastgeleSayi();  // RastgeleSayi sınıfından r nesnesini oluşturduk
+        Console.WriteLine(r.Sayi);
+
+        r.Degistir();
+        Console.WriteLine(r.Sayi);
+
+        Console.ReadKey();
+        }
+    }
+
